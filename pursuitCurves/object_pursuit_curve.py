1f676e37d9dfde708006ada894a60861b9e594e7
Victor <victor.montielargaiz@gmail.com>
2018-12-29 14:39:56 +0100

    Problem configuration has been moved to a dictionary. Added two and three body example problems. Line width has been diferenciated between envolving curve and pursuer-leader object. Fixed problem, the last segment of the envolving curve was not painted

--- pursuitCurves/object_pursuit_curve.py
+++ pursuitCurves/object_pursuit_curve.py
@@ -20,7 +20,7 @@ class ObjectPursuit( pyglet.window.Window ):
         """
         Constructor method
         Arguments:
-            moving_objects: list with moving objects where relationships of
+            moving_objects: dict with moving objects where relationships of
             who is the follower and who the leader has been established            
             size: tuple of ints, size of the window            
             dt: float, step size for the time            
@@ -39,9 +39,12 @@ class ObjectPursuit( pyglet.window.Window ):
         #Save the follower and leader objects
         #The internal relationship between moving object has already been created
         self.moving_objects = {}
-        for m in moving_objects:
+        for name, mover in moving_objects.items():
+            
+            print("Setting up configuration for object: {}".format( name ) )
+            
             #Set random colors to each object
-            self.moving_objects[ m ] = { 'color' : random_color() }
+            self.moving_objects[ mover ] = { 'color' : random_color() }
             
     def __repr__(self):
         """
@@ -145,6 +148,7 @@ class ObjectPursuitImage( ObjectPursuit ):
         if self.envelope:            
             color = (255, 255, 255)
             
+            pyglet.gl.glLineWidth( 0.5 )
             for m, attributes in self.moving_objects.items():                 
                 leader = m.get_leader()
                 if not leader is None:
@@ -162,16 +166,17 @@ class ObjectPursuitImage( ObjectPursuit ):
                         
         #Draw all calculated points for each object as lines between
         #two consecutive states
+        pyglet.gl.glLineWidth( 3.0 )
         for m, attributes in self.moving_objects.items(): 
             
             color = attributes['color']
             
-            for i in range(0, self.max_iter - 1 ):      
+            for i in range(0, self.max_iter - 1):      
                 point_1 = attributes['points'][i]
                 point_2 = attributes['points'][i+1]             
                 pyglet.graphics.draw(2, pyglet.gl.GL_LINES,
                                      ('v3f', (*point_1, *point_2)),
-                                     ('c3B',  color*2) #Define color 
+                                     ('c3B',  color*2) 
                                      )
         
     
@@ -185,7 +190,8 @@ class ObjectPursuitImage( ObjectPursuit ):
         """
         Calculate the all points to create the fixed image
         """        
-        #For each moving object, create empty array with image data
+        #For each moving object, create empty array with image data where
+        #the simulated positions will be stored
         for m, attributes in self.moving_objects.items():
             attributes['points'] = np.zeros( (self.iterations, 3) )
             
@@ -196,13 +202,6 @@ class ObjectPursuitImage( ObjectPursuit ):
             #Flag to indicate all moving objects are close enough to stop simulation
             all_objects_below_closeness = True
             
-            #Update mover positions
-            for m in self.moving_objects:
-                m.update_position( self.dt )
-                
-                #save location of the object at this moment of the simulation
-                self.moving_objects[m]['points'][i] = m.get_position()
-              
             #Check closeness between pursuers and followers
             for m in self.moving_objects:
                 
@@ -215,12 +214,19 @@ class ObjectPursuitImage( ObjectPursuit ):
                     if dist > self.closeness:
                         all_objects_below_closeness = False
             
+            #Update movers positions
+            for m in self.moving_objects:
+                m.update_position( self.dt )
+                
+                #save location of the object at this moment of the simulation
+                self.moving_objects[m]['points'][i] = m.get_position()
+              
             #If pursuers and leaders are close enough, stop simulation at this point
             if all_objects_below_closeness:
                 self.max_iter = i
-                break                
+                break           
                     
-            #Recalculate follower directions
+            #Recalculate follower directions for next iteration
             for m in self.moving_objects:
                 m.update_velocity( )
 
@@ -230,8 +236,6 @@ class ObjectPursuitCurveAnimator(ObjectPursuit, threading.Thread):
     Models a Pursuit Curve problem on a three dimensional space
     with animation
     """
-    
-    key = pyglet.window.key
     
     def __init__(self, moving_objects, size, dt):
         
@@ -292,44 +296,119 @@ class ObjectPursuitCurveAnimator(ObjectPursuit, threading.Thread):
             
 def main():
     
+    #Problem configuration
     k = 2.0 #Speed ratio
     leader_speed = 2.0
     follower_speed = k * leader_speed
     
     #Time between movements updates on the screen
-    DELTA_TIME_SECONDS = 2.0
+    DELTA_TIME_SECONDS = 6.0
     
-    #Create the followers and leaders
-    follower = Mover( "Follower A", (10, 50, 0), (0, 1, 0), follower_speed )
-    leader = Mover( "Leader B", (10, 10, 0), (1, 0.25, 0), leader_speed )
-    follower.set_leader( leader )
-    moving_objects = [follower, leader]
+    #config dict to be exported as a json file
+    #TODO: Move line width and colors to the configuration file
+    
+    #Two body problem
+    config_two_body = {
+            'dt' : DELTA_TIME_SECONDS,
+            'windows_size' : (400, 400),
+            'animation' : False,
+            'iterations' : 1000,
+            'closeness' : DELTA_TIME_SECONDS,
+            'envelope' : True,
+            'frame_per_second' : 20,
+            'movers' : [
+                         { 'name' : 'Follower A',
+                           'r_0' : (10, 250, 0),
+                           'v_0' : (0, 1, 0),
+                           'speed' : follower_speed,
+                           'leader' : 'Leader B'
+                         },
+                         { 'name' : 'Leader B',
+                           'r_0' : (10, 10, 0),
+                           'v_0' : (1, 0.25, 0),
+                           'speed' : leader_speed,
+                           'leader' : None
+                         }
+                      ]
+            }
+            
+    #Two body problem
+    config_three_body = {
+            'dt' : DELTA_TIME_SECONDS,
+            'windows_size' : (400, 400),
+            'animation' : False,
+            'iterations' : 1000,
+            'closeness' : DELTA_TIME_SECONDS / 2,
+            'envelope' : True,
+            'frame_per_second' : 20,
+            'movers' : [
+                         { 'name' : 'A',
+                           'r_0' : (10, 10, 0),
+                           'v_0' : (1, 0, 0),
+                           'speed' : leader_speed,
+                           'leader' : 'B'
+                         },
+                         { 'name' : 'B',
+                           'r_0' : (210, 10, 0),
+                           'v_0' : (1, 0, 0),
+                           'speed' : leader_speed,
+                           'leader' : 'C'
+                         },
+                         { 'name' : 'C',
+                           'r_0' : ( 200 / 2 + 10, 10 + 200 * np.sqrt(3) / 2.0 , 0),
+                           'v_0' : (1, 0, 0),
+                           'speed' : leader_speed,
+                           'leader' : 'A'
+                         }
+                      ]
+            }
+            
+    
+    #Create the followers and leaders from config file
+    config = config_three_body
+    movers = config['movers']
+    moving_objects = { }
+    
+    for mover in movers:        
+        #Create object
+        mover_obj = Mover( name = mover['name'], 
+                      r0 = mover['r_0'], 
+                      v0 = mover['v_0'], 
+                      speed = mover['speed'] )
+        
+        #Store in the moving object dict
+        moving_objects[ mover_obj.get_name() ] = mover_obj
+ 
+    #Create pursuer-leader relationships
+    for m in movers:
+        name = m['name']
+        leader_name = m['leader']
+        if not leader_name is None:      
+            leader = moving_objects[ leader_name ]
+            moving_objects[ name ].set_leader( leader )
+    
    
-    #Create the Pursuit Curve problem
-    win_size = (250, 250)
-    
-    #Animation or fixed image?
-    animation = False
-    
-    if animation:
+    #Create the Pursuit Curve problem depending on whether it is an animation
+    #or a fixed image
+    if config['animation']:
         problem = ObjectPursuitCurveAnimator(moving_objects, 
-                                        size = win_size, 
-                                        dt = DELTA_TIME_SECONDS)
+                                        size = config['windows_size'], 
+                                        dt = config['dt'])
     else:
         problem = ObjectPursuitImage(moving_objects, 
-                                        size = win_size, 
-                                        dt = DELTA_TIME_SECONDS,
-                                        iterations = 100,
-                                        closeness = 1.5,
-                                        envelope = True)
+                                        size = config['windows_size'], 
+                                        dt = config['dt'],
+                                        iterations = config['iterations'],
+                                        closeness = config['closeness'],
+                                        envelope = config['envelope'] )
     
     #Force rendering of screen at start up
     problem.dispatch_events()
     problem.dispatch_event('on_draw')
-    time.sleep( DELTA_TIME_SECONDS )
+    time.sleep( config['dt'] )
         
     #Set Frame Rate
-    pyglet.clock.set_fps_limit( 20 )
+    pyglet.clock.set_fps_limit( config['frame_per_second'] )
 
     #Start the pursuit simulation    
     problem.start()
@@ -348,11 +427,3 @@ if __name__ == '__main__':
   main()
   
 
-#TODO: Implement antialiasing
-#TODO: define a configuration file where we can define objects
-  
-###References
-#http://www.mat.ucm.es/cosasmdg/cdsmdg/modelizaciones/proyectos/proyecto2/index.htm
-#https://fr.wikipedia.org/wiki/Courbe_du_chien
-#https://en.wikipedia.org/wiki/Pursuit_curve
-#http://mathworld.wolfram.com/PursuitCurve.html
